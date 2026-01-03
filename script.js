bb64ab90c2166192be5381020bcfa303ad5349fc
samyakjain09 <samyakjain1p@gmail.com>
2026-01-03 22:49:01 +0530

    react basic

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-import name from './app.js'
-console.log(name);
+var h1=React.createElement("h1",null,"hello World")
+var h2=React.createElement("h2",null,"hello samyak")
+
+var div=React.createElement("div",{id:parent},[h1,h2])
+
+var samyak=ReactDOM.createRoot(document.querySelector("#box"))
+
+samyak.render(div);
