461afc115e73a70ce3ad0d78822f72d34f2605d2
samyakjain09 <samyakjain1p@gmail.com>
2026-01-04 22:52:41 +0530

    message

--- script.js
+++
@@ -1,8 +0,0 @@
-var h1=React.createElement("h1",null,"hello World")
-var h2=React.createElement("h2",null,"hello samyak")
-
-var div=React.createElement("div",{id:parent},[h1,h2])
-
-var samyak=ReactDOM.createRoot(document.querySelector("#box"))
-
-samyak.render(div);
